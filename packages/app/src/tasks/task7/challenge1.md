047a9c1d2553439a6eef247d3733e6bde8620bb2
David Nascimento <davidpn.11@gmail.com>
2022-01-24 23:32:20 -0700

    write RDS questions

--- packages/app/src/tasks/task7/challenge1.md
+++ packages/app/src/tasks/task7/challenge1.md
@@ -1,3 +1,30 @@
 ## Challenge 7 - RemoteData: The Sequel
 
-In Progress
+### Introduction
+
+Wow! Look how far have you come! Great job!
+And now to solidify your learnings, we will dedust one of our favourite type: `RemoteData`!
+
+We will take what we learned from the last modules and top up our module a notch.
+
+---
+
+### Challenge 1 - `anyFailures`
+
+For this challenge we will created a helper to detect if there are any `failure` cases in an array of `RemoteData`'s.
+
+We will return an `Option` type, as `some` of the first `Error` found in the array and `none`, in case there are no `failure`s.
+
+---
+
+### Challenge 2 - `successMap`
+
+We require `map` method, to allow us to modify the `result` of a `success` case, without much concern if it's found in another state. Therefore, in case is not a `success`, it should return itself.
+
+---
+
+### Challenge 3 - `Eq and Ord`
+
+Create the `eqRD` and an `ordRD` classes.
+On the `eq` case, we are required to compose it by another `eq`, to compare the results on a `success` case. The same should be done for `ord`.
+The order elsewhere is `success`, `loading`, `initial` and `failure`, respectively.
---
+++ packages/app/src/tasks/task7/remote-data.ts
@@ -0,0 +1,80 @@
+import * as O from 'fp-ts/lib/Option';
+
+type RemoteDataInitial = {tag: 'initial'};
+type RemoteDataLoading = {tag: 'loading'};
+type RemoteDataSuccess<A> = {tag: 'success'; result: A};
+type RemoteDataFailure<E> = {tag: 'failure'; error: E};
+
+export type RemoteData<A, E = Error> =
+  | RemoteDataInitial
+  | RemoteDataLoading
+  | RemoteDataSuccess<A>
+  | RemoteDataFailure<E>;
+
+export let initial: () => RemoteData<never, never>;
+export let loading: () => RemoteData<never, never>;
+export let success: <A, E = Error>(result: A) => RemoteData<A, E>;
+export let failure: <A, E = Error>(error: E) => RemoteData<A, E>;
+
+export let isInitial: any;
+export let isLoading: any;
+export let isSuccess: any;
+export let isFailure: any;
+
+export let fold: <A, B, E>(
+  whenInitial: () => B,
+  whenLoading: () => B,
+  whenSuccess: (result: A) => B,
+  whenFailure: (error: E) => B,
+) => (rd: RemoteData<A, E>) => B;
+
+export let successOrElse: <A, B, E>(
+  onSuccess: (a: A) => B,
+  orElse: () => B,
+) => (rd: RemoteData<A, E>) => B;
+
+export let failureOrElse: <A, E, B>(
+  onFailure: (e: E) => B,
+  orElse: () => B,
+) => (rd: RemoteData<A, E>) => B;
+
+export let anyFailures: <E = Error>(p: RemoteData<unknown, E>[]) => O.Option<E>;
+
+export let successMap: <A, B>(
+  f: (a: A) => B,
+) => (fa: RemoteData<A, unknown>) => RemoteData<B, unknown>;
+
+/**
+ * Solutions
+ */
+
+initial = () => ({tag: 'initial'});
+loading = () => ({tag: 'loading'});
+success = result => ({tag: 'success', result});
+failure = error => ({tag: 'failure', error});
+
+//@ts-ignore
+isInitial = rd => rd.tag === 'initial';
+//@ts-ignore
+isLoading = rd => rd.tag === 'loading';
+//@ts-ignore
+isSuccess = rd => rd.tag === 'success';
+//@ts-ignore
+isFailure = rd => rd.tag === 'failure';
+
+fold =
+  <B, A, E>(
+    whenInitial: () => B,
+    whenLoading: () => B,
+    whenSuccess: (result: A) => B,
+    whenFailure: (error: E) => B,
+  ) =>
+  (rd: RemoteData<A, E>): B => {
+    if (rd.tag === 'initial') return whenInitial();
+    if (rd.tag === 'loading') return whenLoading();
+    if (rd.tag === 'success') return whenSuccess(rd.result);
+    return whenFailure(rd.error);
+  };
+
+successOrElse = (onSuccess, onElse) => fold(onElse, onElse, onSuccess, onElse);
+failureOrElse = (onFailure, onElse) => fold(onElse, onElse, onElse, onFailure);

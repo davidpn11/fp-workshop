99b47bdd7af75341a1bea81e77766b6f7dba18dd
David Nascimento <davidpn.11@gmail.com>
2022-01-28 10:57:28 -0700

    migrate ord without solution

--- packages/app/src/tasks/task6/challenge1.md
+++ packages/app/src/tasks/task6/challenge1.md
@@ -1,3 +1,33 @@
 ## Challenge 6 - Eq and Sort
 
-In Progress
+---
+
+### Introduction
+
+---
+
+### Promotions Eq/Sort
+
+**Challenge 1.a (1 to 3)** - `Eq.Eq<Promotion>`
+
+Create and Eq type for Promotions called `eqPromotions`. if the promotion is of type `TOP_PLACEMENT` only compare the id.
+If is `FREE_ITEM` compare the promotion id and the item name
+
+---
+
+**Challenge 1.b** - `( promotions: Promotion[], deletedPromotions: Promotion[], ) => Promotion[];`
+
+This type takes as a input `promotions` and `deletedPromotions`, both being lists of `Promotions`. Create a method called `removeDeletedPromotions`. It returns the `promotions` list, but removing all the promotions that also exist in `deletedPromotions`.
+
+---
+
+**Challenge 1.c** - `Ord.Ord<Promotion>`
+
+Create a Ord type for Promotions `ordPromotions`. The rules to sort promotions are:
+
+- `TOP_PLACEMENT` promotions should come first than `FREE_ITEM`
+- if it has budget, sort by `totalAmount`
+
+> This will be used to sort an array of `Promotions`
+
+---
--- packages/app/src/tasks/task6/index.tsx
+++ packages/app/src/tasks/task6/index.tsx
@@ -1,35 +1,260 @@
-import {identity} from 'fp-ts/lib/function';
+import {ord} from 'fp-ts';
+import {flow, identity} from 'fp-ts/lib/function';
 import React from 'react';
 import {TaskSystem} from '../../components/TaskSystem';
 import {TaskPage} from '../style';
 import Challenge1Markdown from './challenge1.md';
+import Challenge2Markdown from './challenge2.md';
+import {
+  eqOrder,
+  eqPromotion,
+  ordOrder,
+  ordPromotions,
+  removeDeletedPromotions,
+  recordToOrderList,
+  filterDuplicatedOrder,
+} from './challenges';
+import {
+  promosArray,
+  deletedPromosArray,
+  ordersArray1,
+  ordersArray2,
+  ordersArray3,
+  date1,
+  date2,
+  date3,
+  date5,
+  ordersRecord,
+  orderedRecordList,
+} from './types';
+import * as A from 'fp-ts/lib/Array';
 
-const challenge1a: Challenge = {
-  id: 'Challenge 1.a',
-  input: {},
+const challenge1a1: Challenge = {
+  id: 'Challenge 1.a (1)',
+  input: {promo1: promosArray[0], promo2: deletedPromosArray[0]},
   inputType: 'json',
-  expectedOutput: {},
+  paramsType: 'spread',
+  expectedOutput: true,
+  outputType: 'primitive',
+  status: 'neutral',
+  handler: eqPromotion.equals,
+};
+
+const challenge1a2: Challenge = {
+  id: 'Challenge 1.a (2)',
+  input: {promo1: promosArray[1], promo2: promosArray[2]},
+  inputType: 'json',
+  paramsType: 'spread',
+  expectedOutput: false,
+  outputType: 'primitive',
+  status: 'neutral',
+  handler: eqPromotion.equals,
+};
+
+const challenge1a3: Challenge = {
+  id: 'Challenge 1.a (3)',
+  input: {promo1: promosArray[1], promo2: deletedPromosArray[1]},
+  inputType: 'json',
+  paramsType: 'spread',
+  expectedOutput: true,
+  outputType: 'primitive',
+  status: 'neutral',
+  handler: eqPromotion.equals,
+};
+
+const challenge1b: Challenge = {
+  id: 'Challenge 1.b',
+  input: {promotions: promosArray, deletedPromotions: deletedPromosArray},
+  inputType: 'json',
+  paramsType: 'spread',
+  expectedOutput: [
+    {
+      id: 'promo2',
+      type: 'FREE_ITEM',
+      item: {
+        name: 'Soda Pop',
+        price: 1.5,
+      },
+    },
+    {
+      id: 'promo5',
+      type: 'TOP_PLACEMENT',
+    },
+    {
+      id: 'promo6',
+      type: 'FREE_ITEM',
+      item: {
+        name: 'Fried Gyoza',
+        price: 5.5,
+      },
+    },
+  ],
   outputType: 'json',
   status: 'neutral',
-  handler: identity,
+  handler: removeDeletedPromotions,
+};
+
+const challenge1c: Challenge = {
+  id: 'Challenge 1.c',
+  input: promosArray,
+  inputType: 'json',
+  expectedOutput: [
+    {
+      id: 'promo1',
+      type: 'TOP_PLACEMENT',
+      budget: {
+        totalAmount: 100,
+        consumedAmount: 25,
+      },
+    },
+    {
+      id: 'promo5',
+      type: 'TOP_PLACEMENT',
+    },
+    {
+      id: 'promo2',
+      type: 'FREE_ITEM',
+      item: {
+        name: 'Fries',
+        price: 3.5,
+      },
+      budget: {
+        totalAmount: 50,
+        consumedAmount: 0,
+      },
+    },
+    {
+      id: 'promo2',
+      type: 'FREE_ITEM',
+      item: {
+        name: 'Soda Pop',
+        price: 1.5,
+      },
+    },
+    {
+      id: 'promo6',
+      type: 'FREE_ITEM',
+      item: {
+        name: 'Fried Gyoza',
+        price: 5.5,
+      },
+    },
+  ],
+  outputType: 'json',
+  status: 'neutral',
+  handler: flow(A.sort(ordPromotions)),
+};
+
+const challenge2a1: Challenge = {
+  id: 'Challenge 2.a (1)',
+  input: {order1: ordersArray1[0], order2: ordersArray2[0]},
+  inputType: 'json',
+  paramsType: 'spread',
+  expectedOutput: false,
+  outputType: 'primitive',
+  status: 'neutral',
+  handler: eqOrder.equals,
+};
+
+const challenge2a2: Challenge = {
+  id: 'Challenge 2.a (2)',
+  input: {order1: ordersArray1[0], order2: ordersArray2[1]},
+  inputType: 'json',
+  paramsType: 'spread',
+  expectedOutput: false,
+  outputType: 'primitive',
+  status: 'neutral',
+  handler: eqOrder.equals,
+};
+
+const challenge2a3: Challenge = {
+  id: 'Challenge 2.a (3)',
+  input: {order1: ordersArray1[1], order2: ordersArray2[2]},
+  inputType: 'json',
+  paramsType: 'spread',
+  expectedOutput: true,
+  outputType: 'primitive',
+  status: 'neutral',
+  handler: eqOrder.equals,
+};
+
+const challenge2b: Challenge = {
+  id: 'Challenge 2.b',
+  input: ordersArray3,
+  inputType: 'json',
+  expectedOutput: [
+    {
+      customerId: '1',
+      orderDate: date1,
+      totalPrice: 30.5,
+    },
+    {
+      customerId: '1',
+      orderDate: date3,
+      promotionId: 'promo1',
+      totalPrice: 20.5,
+    },
+    {
+      customerId: '2',
+      orderDate: date2,
+      promotionId: 'promo2',
+      totalPrice: 50.5,
+    },
+    {
+      customerId: '3',
+      orderDate: date5,
+      totalPrice: 40,
+    },
+  ],
+  outputType: 'json',
+  status: 'neutral',
+  handler: filterDuplicatedOrder,
+};
+
+const challenge2c: Challenge = {
+  id: 'Challenge 2.c',
+  input: ordersRecord,
+  inputType: 'json',
+  expectedOutput: orderedRecordList,
+  outputType: 'json',
+  status: 'neutral',
+  handler: recordToOrderList,
 };
 
 const challengeSet1: ChallengeSet = {
   title: 'Challenge 1.a',
   markdown: Challenge1Markdown.markdown,
-  challenges: [challenge1a],
+  challenges: [
+    challenge1a1,
+    challenge1a2,
+    challenge1a3,
+    challenge1b,
+    challenge1c,
+  ],
 };
 
-const task3: TaskType = {
-  title: 'task3',
-  sets: [challengeSet1],
+const challengeSet2: ChallengeSet = {
+  title: 'Challenge 2.a',
+  markdown: Challenge1Markdown.markdown,
+  challenges: [
+    challenge2a1,
+    challenge2a2,
+    challenge2a3,
+    challenge2b,
+    challenge2c,
+  ],
+};
+
+const task6: TaskType = {
+  title: 'task6',
+  sets: [challengeSet1, challengeSet2],
   currentSet: challengeSet1,
 };
 
 export function Task6() {
   return (
     <TaskPage>
-      <TaskSystem task={task3} />
+      <TaskSystem task={task6} />
     </TaskPage>
   );
 }
